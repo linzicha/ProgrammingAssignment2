6548233a43a8970fa9c0f417631842e189627d72
linzicha <linzi@kean.edi>
2022-06-16 15:08:19 +0800

    Create Untitled.R

---
+++ Untitled.R
@@ -0,0 +1,29 @@
+##我只是将输入 x 设置为矩阵,然后将求解的值“s”设置为空,然后我把每一个对“mean”的引用都改成了“solve”
+
+makeCacheMatrix <- function(x = matrix()) {
+  j <- NULL
+  set <- function(y){
+    x <<- y
+    j <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) j <<- inverse
+  getInverse <- function() j 
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
+}
+##这里也一样，把“mean”改成“solve”，“m”改成“s”
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  j <- x$getInverse()
+  if(!is.null(j)){
+    message("getting cached data")
+    return(j)
+  }
+  mat <- x$get()
+  j <- solve(mat,...)
+  x$setInverse(j)
+  j
+}
